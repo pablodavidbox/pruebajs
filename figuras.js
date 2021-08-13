e4abdc79401d755cdd330b08ad97f6d746bf8f13
pablodavidbox <pablodavidseller@hotmail.com>
2021-08-13 17:27:45 -0300

    codigo de figuras geometricas

---
+++ figuras.js
@@ -0,0 +1,31 @@
+// Codigo del Cuadrado 
+console.group("Cuadrados");
+const ladoCuadrado = 5;
+
+console.log("Los lados del cuadrado miden: "+ladoCuadrado);
+
+const perimetroCuadrado = ladoCuadrado * 4;
+
+console.log("El perimetro del cuadrado es:"+perimetroCuadrado);
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+console.log("El area del cuadrado es:"+areaCuadrado);
+
+console.groupEnd();
+// Codigo del Triangulo 
+console.group("Triangulo");
+const ladoTriangulo1 = 6; 
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+
+console.log("Los lados del triangulo miden: Lado 1"
+                 +ladoTriangulo1
+                 +" Lado2 "
+                 +ladoTriangulo2
+                 + "y la base "
+                 +baseTriangulo
+            );
+
+console.groupEnd();

d4ae7e9bb497c7b7e3020e4c5f536c7a5f895225
Pablo David Schvager <pablodavidseller@hotmail.com>
2021-08-15 15:38:33 -0300

    agregamos otras operaciones del triangulo

--- figuras.js
+++ figuras.js
@@ -20,6 +20,7 @@ const ladoTriangulo2 = 6;
 const baseTriangulo = 4;
 
 
+
 console.log("Los lados del triangulo miden: Lado 1"
                  +ladoTriangulo1
                  +" Lado2 "
@@ -27,5 +28,18 @@ console.log("Los lados del triangulo miden: Lado 1"
                  + "y la base "
                  +baseTriangulo
             );
+
+
+const alturaTriangulo = 5.5;
+console.log("La altura del trianguloes:"+alturaTriangulo);
+
+const perimetroTriangulo = ladoTriangulo1+ladoTriangulo2+baseTriangulo;
+
+console.log("El perimetro del triangulo es:"+perimetroTriangulo);
+
+const areaTriangulo = (baseTriangulo*alturaTriangulo)/2;
+
+console.log("El area del triangulo es:"+areaTriangulo);
+
 
 console.groupEnd();

2c6088cf69666300921eb75bbba4338478afda93
pablodavidbox <pablodavidseller@hotmail.com>
2021-08-19 13:43:46 -0300

    agregamos conjunto de datos de salarios de personas en el arreglo de nombre Argentina

---
+++ salarios.js
@@ -0,0 +1,141 @@
+const argentina = [];
+
+colombia.push(
+    {
+        name: "Juan",
+        salary: 1000,
+    }
+);
+
+colombia.push(
+    {
+        name: "Natalia",
+        salary: 1200,
+    }
+);
+
+colombia.push(
+    {
+        name: "Pablo",
+        salary: 1200,
+    }
+);
+
+colombia.push(
+    {
+        name: "Nora",
+        salary: 1200,
+    }
+);
+
+colombia.push(
+    {
+        name: "Ricardo",
+        salary: 1200,
+    }
+);
+
+colombia.push(
+    {
+        name: "Miguel",
+        salary: 300,
+    }
+);
+
+colombia.push(
+    {
+        name: "Rolando",
+        salary: 500,
+    }
+);
+
+colombia.push(
+    {
+        name: "Fransisco",
+        salary: 6000,
+    }
+);
+
+colombia.push(
+    {
+        name: "Samuel",
+        salary: 12000,
+    }
+);
+
+colombia.push(
+    {
+        name: "Fernando",
+        salary: 900,
+    }
+);
+
+colombia.push(
+    {
+        name: "Peretti",
+        salary: 1000,
+    }
+);
+
+colombia.push(
+    {
+        name: "Tatiana",
+        salary: 1200,
+    }
+);
+
+colombia.push(
+    {
+        name: "Simon",
+        salary: 1200,
+    }
+);
+
+colombia.push(
+    {
+        name: "Leon",
+        salary: 1200,
+    }
+);
+
+colombia.push(
+    {
+        name: "Daniel",
+        salary: 1200,
+    }
+);
+
+colombia.push(
+    {
+        name: "Maximo",
+        salary: 300,
+    }
+);
+
+colombia.push(
+    {
+        name: "Joel",
+        salary: 500,
+    }
+);
+
+colombia.push(
+    {
+        name: "Peron",
+        salary: 6000,
+    }
+);
+
+colombia.push(
+    {
+        name: "Ramuel",
+        salary: 12000,
+    }
+);
+
+colombia.push(
+    {
+        name: "Mariana",
+        salary: 900,
+    }
+);

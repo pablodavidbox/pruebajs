c81305ee3f69e733fd30fba5acde7923e8bb094d
pablodavidbox <pablodavidseller@hotmail.com>
2021-08-22 17:24:38 -0300

    Agregue algunas funciones al archivo analisis.js

--- analisis.js
+++ analisis.js
@@ -5,4 +5,16 @@ const salariosArg = argentina.map(
 
 );
 
-const salariosArgSorted = 
+const salariosArgSorted = salariosArg.sort(
+    function (salaryA, salaryB) {
+               salaryA - salaryB;
+    }
+);
+
+
+
+////////////////////////////////
+// Te dice si es un numero par
+function espar(number){
+    return (number % 2  === 0);    
+}

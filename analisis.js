0a627cd471a5907d7c8bc89b914ebf71c713365c
pablodavidbox <pablodavidseller@hotmail.com>
2021-08-23 13:59:17 -0300

    elimine comentarios

--- analisis.js
+++ analisis.js
@@ -7,7 +7,6 @@ const salariosArg = argentina.map(
 
 const salariosArgSorted = salariosArg.sort(
     function (salaryA, salaryB) {
-<<<<<<< HEAD
            return salaryA - salaryB;
     }
 );
@@ -17,5 +16,4 @@ const salariosArgSorted = salariosArg.sort(
 // Te dice si es un numero par
 function espar(number){
     return (number % 2  === 0);    
->>>>>>> c81305ee3f69e733fd30fba5acde7923e8bb094d
 }

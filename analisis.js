fd8f6647624b1a8588296b7502aab47cf1d2da19
pablodavidbox <pablodavidseller@hotmail.com>
2021-08-23 14:35:13 -0300

    Hice correcciones en el codigo

--- analisis.js
+++ analisis.js
@@ -18,15 +18,35 @@ function esPar(number){
     return (number % 2  === 0);    
 }
 
+////////////////////////////////////////////////////////////////////////
+function calcularMediaAritmetica(lista) {
+    const sumaLista = lista.reduce(
+      function (valorAcumulado = 0, nuevoElemento) {
+        return valorAcumulado + nuevoElemento;
+      }
+    );
+    const promedioLista = sumaLista / lista.length;
+    return promedioLista;
+}
+
+////////////////////////////////////////////////////////////////////////
+
 function medianaSalarios(lista) {
     const mitad = parseInt(lista.length/2);
 
-    if(esPar(list.length)){
+    if(esPar(lista.length)){
         const personitaMitad1 = lista[mitad - 1];
         const personitaMitad2 = lista[mitad];
+        const mediana = calcularMediaAritmetica([personitaMitad1, personitaMitad2]);
+        return mediana;
     }else{
         const personitaMitad = lista[mitad];
         return personitaMitad;
     }
     
 }
+
+
+console.log(
+    medianaSalarios(salariosArgSorted)
+)
--- salarios.js
+++ salarios.js
@@ -1,139 +1,139 @@
 const argentina = [];
 
-colombia.push(
+argentina.push(
     {
         name: "Juan",
         salary: 1000,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Natalia",
         salary: 1200,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Pablo",
         salary: 1200,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Nora",
         salary: 1200,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Ricardo",
         salary: 1200,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Miguel",
         salary: 300,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Rolando",
         salary: 500,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Fransisco",
         salary: 6000,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Samuel",
         salary: 12000,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Fernando",
         salary: 900,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Peretti",
         salary: 1000,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Tatiana",
         salary: 1200,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Simon",
         salary: 1200,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Leon",
         salary: 1200,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Daniel",
         salary: 1200,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Maximo",
         salary: 300,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Joel",
         salary: 500,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Peron",
         salary: 6000,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Ramuel",
         salary: 12000,
     }
 );
 
-colombia.push(
+argentina.push(
     {
         name: "Mariana",
         salary: 900,

a47677b98c2e594775846fb005b5229b58542062
pablodavidbox <pablodavidseller@hotmail.com>
2021-08-23 13:30:00 -0300

    algunos cambios en analisis

--- analisis.js
+++ analisis.js
@@ -5,4 +5,17 @@ const salariosArg = argentina.map(
 
 );
 
-const salariosArgSorted = 
+const salariosArgSorted = salariosArg.sort(
+    function (salaryA, salaryB) {
+           return salaryA - salaryB;
+    }
+);
+
+function esPar(number) {
+
+    if( number % 2 === 0){
+             return true;
+    }else{
+             return false;
+    }
+}
